1565fbc851250e7237c218cd07c6324bd847a883
Kardel123 <154974043+Kardel123@users.noreply.github.com>
2025-02-18 14:21:59 +0800

    Feat: Making page responsive and Integrating Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
