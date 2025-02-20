0cba0cf35ce09a7d469aa927c6bce152b0f662fb
Kardel123 <154974043+Kardel123@users.noreply.github.com>
2025-02-20 21:48:35 +0800

    Ekoms King Posa

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
 
 Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/items', [ItemController::class, 'store'])->name('items.store');
+Route::get('/inventory/create', [ItemController::class, 'create'])->name('items.create');
+Route::post('/inventory', [ItemController::class, 'store'])->name('items.store');
