6fb86404a2c754ae2c19911afd560ea11c61a424
Kardel123 <154974043+Kardel123@users.noreply.github.com>
2025-03-13 19:17:41 +0800

    Add Category model and display categories data using Laravel directives

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
@@ -12,3 +13,4 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/items', [ItemController::class, 'store'])->name('items.store');
 Route::get('/inventory/create', [ItemController::class, 'create'])->name('items.create');
 Route::post('/inventory', [ItemController::class, 'store'])->name('items.store');
+Route::get('/categories', [CategoryController::class, 'index']);
